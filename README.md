72d777f1a2cc4c4bc72493b965f4605b760365f3
NatPDeveloper <you@example.com>
2022-09-06 15:20:19 -0400

    add

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 AntelopeIO Docker files
 
 Dockerfiles for leap & cdt.
+
+Directory:
+- dfuseeos - StreamingFast AntelopIO dfuseeos binary
+- leap-cdt - AntelopeIO leap nodeos daemon and cdt contract dev toolkit
+- zeus - DAPP Network Zeus-sdk docker used for docker backup
--- scripts/build_deploy.sh
+++ scripts/build_deploy.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
-sudo docker build -t natpdev/leap-cdt .
+sudo docker build -t natpdev/leap-cdt ./leap-cdt
+sudo docker build -t natpdev/dfuseeos ./dfuseeos
 sudo docker build -t natpdev/zeus ./zeus
 sudo docker push natpdev/leap-cdt
+sudo docker push natpdev/dfuseeos
 sudo docker push natpdev/zeus
